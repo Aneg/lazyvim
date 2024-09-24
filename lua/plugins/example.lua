b62fa0932a2b437d99f61e53fd9b431c51c68158
bg <gennadyborisenko@gmail.com>
2024-09-25 00:24:47 +0300

    fix plugins

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -425,23 +425,23 @@ return {
       { "<leader>de", function() require("dapui").eval() end, desc = "[d]ap [e]val" },
     },
   },
-  {
-  "kdheepak/lazygit.nvim",
-  cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
-  },
-  -- optional for floating window border decoration
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  keys = {
-    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-  }
-},
+--   {
+--   "kdheepak/lazygit.nvim",
+--   cmd = {
+--     "LazyGit",
+--     "LazyGitConfig",
+--     "LazyGitCurrentFile",
+--     "LazyGitFilter",
+--     "LazyGitFilterCurrentFile",
+--   },
+--   -- optional for floating window border decoration
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--   },
+--   -- setting the keybinding for LazyGit with 'keys' is recommended in
+--   -- order to load the plugin when the command is run for the first time
+--   keys = {
+--     { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+--   }
+-- },
 }
