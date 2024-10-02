bd1129b9f7bc21d09b75e6673c2e4eb891a3d2e8
bg <gennadyborisenko@gmail.com>
2024-10-02 09:16:26 +0300

    разнёс по файлам

---
+++ lua/plugins/mason.nvim.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "goimports",
+        "gofumpt",
+        "gomodifytags",
+        "impl",
+        "delve",
+        "sqlfluff",
+      },
+    },
+  },
+}
